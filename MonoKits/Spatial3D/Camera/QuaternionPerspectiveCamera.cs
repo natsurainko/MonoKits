ee6ce2585e77a76cc5b01901b137885cfacc299c
XiaoLuDS <460556668@qq.com>
2025-10-31 15:35:25 +0800

    add function lerp, add enum named Object3DRestriction
    this function made the Quaternion Perspective Camera following smoothly, which combines with both camera mode and move speed.
    the Object3DRestriction will transform the behavior of function Move() and Rotate() in class GameObject3D

--- MonoKits/Spatial3D/Camera/QuaternionPerspectiveCamera.cs
+++ MonoKits/Spatial3D/Camera/QuaternionPerspectiveCamera.cs
@@ -8,10 +8,17 @@ public class QuaternionPerspectiveCamera(ViewportAdapter viewportAdapter) : Game
     private readonly ViewportAdapter _viewportAdapter = viewportAdapter;
     private GameObject3D? _targetObject;
 
-    private Quaternion _rotationFromWorldToCamera = Quaternion.Identity;
+    private Vector3 _positionNow = Vector3.Zero;
+
+    private Quaternion _quaternionTarget = Quaternion.Identity;
+    private Quaternion _quaternionNow = Quaternion.Identity;
+
+    private bool _usePositionLerp = false;
 
     private Vector3 _movementAccumulator = Vector3.Zero;
     private Vector3 _rotationAccumulator = Vector3.Zero;
+
+    private CameraMode oldCameraMode = CameraMode.Free;
 
     public float FieldOfView { get; set; } = MathHelper.ToRadians(45);
     public float AspectRatio => _viewportAdapter.Viewport.AspectRatio;
@@ -20,9 +27,13 @@ public class QuaternionPerspectiveCamera(ViewportAdapter viewportAdapter) : Game
     public CameraMode CameraMode { get; set; } = CameraMode.Free;
     public float BaseTargetDistance { get; set; } = 0.0f;
 
-    public Vector3 Forward => Vector3.Transform(Vector3.Forward, _rotationFromWorldToCamera);
-    public Vector3 Right => Vector3.Transform(Vector3.Right, _rotationFromWorldToCamera);
-    public Vector3 Up => Vector3.Transform(Vector3.Up, _rotationFromWorldToCamera);
+    private Vector3 ForwardTarget => Vector3.Transform(Vector3.Forward, _quaternionTarget);
+    private Vector3 RightTarget => Vector3.Transform(Vector3.Right, _quaternionTarget);
+    private Vector3 UpTarget => Vector3.Transform(Vector3.Up, _quaternionTarget);
+    private Vector3 ForwardNow => Vector3.Transform(Vector3.Forward, _quaternionNow);
+    private Vector3 RightNow => Vector3.Transform(Vector3.Right, _quaternionNow);
+    private Vector3 UpNow => Vector3.Transform(Vector3.Up, _quaternionNow);
+
 
     /// <param name="offset">ypr = (forward, up, right)</param>
     public override void Move(Vector3 offset)
@@ -40,18 +51,18 @@ public class QuaternionPerspectiveCamera(ViewportAdapter viewportAdapter) : Game
     public void RotateByAxis(Vector3 axis, float angle)
     {
         Quaternion rotation = Quaternion.CreateFromAxisAngle(axis, angle);
-        _rotationFromWorldToCamera = rotation * _rotationFromWorldToCamera;
+        _quaternionTarget = rotation * _quaternionTarget;
     }
-    public void RotateByEuler(float yaw, float pitch, float roll)
+    public void RotateByEuler(float pitch, float yaw, float roll)
     {
         RotateByAxis(Vector3.Up, yaw);
-        RotateByAxis(Right, pitch);
-        RotateByAxis(Forward, roll);
+        RotateByAxis(RightTarget, pitch);
+        RotateByAxis(ForwardTarget, roll);
     }
-    public void RotateToEluer(float yaw, float pitch, float roll)
+    public void RotateToEluer(float pitch, float yaw, float roll)
     {
-        _rotationFromWorldToCamera = Quaternion.Identity;
-        RotateByEuler(yaw, pitch, roll);
+        _quaternionTarget = Quaternion.Identity;
+        RotateByEuler(pitch, yaw, roll);
     }
 
     public void Target(GameObject3D? target) => _targetObject = target;
@@ -59,50 +70,81 @@ public class QuaternionPerspectiveCamera(ViewportAdapter viewportAdapter) : Game
     public void GetProjectionMatrix(out Matrix matrix) => matrix = Matrix.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearPlane, FarPlane);
     public void GetViewMatrix(out Matrix matrix) 
     {
-        if (_rotationAccumulator != Vector3.Zero)
+        if (oldCameraMode != CameraMode)
+        {
+            _usePositionLerp = true;
+            oldCameraMode = CameraMode;
+        }
+
+        UpdateRotation();
+        Vector3 targetPosition = UpdatePosition();
+        matrix = Matrix.CreateLookAt(_positionNow, targetPosition, UpNow);
+    } 
+
+    private void UpdateRotation()
+    {
+        if (CameraMode == CameraMode.FirstPerson && _targetObject != null)
+        {
+            RotateToEluer(_targetObject.Rotation.X, _targetObject.Rotation.Y, _targetObject.Rotation.Z);
+        }
+        else if (_rotationAccumulator != Vector3.Zero)
         {
             base.Rotate(_rotationAccumulator);
-
-            //RotateByEuler(_rotationAccumulator.Y, _rotationAccumulator.X, _rotationAccumulator.Z);
-            RotateToEluer(Rotation.Y, Rotation.X, Rotation.Z);
+            RotateToEluer(Rotation.X, Rotation.Y, Rotation.Z);
 
             _rotationAccumulator = Vector3.Zero;
         }
 
+        //旋转插值
+        if (_quaternionNow != _quaternionTarget) _quaternionNow = Quaternion.Lerp(_quaternionNow, _quaternionTarget, 0.5f);
+    }
+    private Vector3 UpdatePosition()
+    {
         Vector3 targetPosition;
-
         if (CameraMode == CameraMode.FirstPerson && _targetObject != null)
         {
             Position = _targetObject.Position;
-            targetPosition = _targetObject.Position + Forward;
+            targetPosition = _targetObject.Position + ForwardNow;
         }
         else if (CameraMode == CameraMode.ThirdPerson && _targetObject != null)
         {
-            Position = _targetObject.Position - Forward * BaseTargetDistance;
+            Position = _targetObject.Position - ForwardNow * BaseTargetDistance;
             targetPosition = _targetObject.Position;
         }
         else
         {
             if (_movementAccumulator != Vector3.Zero)
             {
-                Position += Forward * _movementAccumulator.X;
-                Position += Right * _movementAccumulator.Z;
-                Position += Up * _movementAccumulator.Y;
+                Position += ForwardNow * _movementAccumulator.X;
+                Position += RightNow * _movementAccumulator.Z;
+                Position += UpNow * _movementAccumulator.Y;
 
                 _movementAccumulator = Vector3.Zero;
             }
 
-            targetPosition = Position + Forward;
-
+            targetPosition = Position + ForwardNow;
         }
 
-        matrix = Matrix.CreateLookAt(Position, targetPosition, Up);
-    } 
+        //切换人称时或高速移动时，移动插值
+        float d = (_positionNow - Position).Length();
+        if (d > 5) _usePositionLerp = true;
+        if (_usePositionLerp && d > 0.05f)
+        {
+            _positionNow = Vector3.Lerp(_positionNow, Position, 0.1f);
+        }
+        else
+        {
+            _usePositionLerp = false;
+            _positionNow = Position;
+        }
+
+        return targetPosition;
+    }
 
     public void Reset()
     {
-        Position = Vector3.Zero;
+        _positionNow = Position = Vector3.Zero;
         Rotation = Vector3.Zero;
-        _rotationFromWorldToCamera = Quaternion.Identity;
+        _quaternionNow = _quaternionTarget = Quaternion.Identity;
     }
 }
---
+++ MonoKits/Spatial3D/Object3DRestriction.cs
@@ -0,0 +1,16 @@
+﻿namespace MonoKits.Spatial3D;
+public enum Object3DRestriction
+{
+    DisForward = 1,
+    DisRight = 1 << 1,
+    DisUp = 1 << 2,
+    DisYaw = 1 << 3,
+    DisPitch = 1 << 4,
+    DisRoll = 1 << 5,
+
+    LimitYaw = 1 << 6,
+    LimitPitch = 1 << 7,
+    LimitRoll = 1 << 8,
+
+    Other = 1 << 9
+}
